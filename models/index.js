cb7dbe664d7c12f9b7d156d352970ab62a89fee4
StevenM <stevenbmendoza@gmail.com>
2022-05-01 10:57:32 -0700

    product to tag associations

--- models/index.js
+++ models/index.js
@@ -13,8 +13,17 @@ Category.hasMany(Product, {
   foreignKey: 'category_id'
 });
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  as: 'product_tags',
+  foreignKey: 'product_id'
+})
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  as: 'product_tags',
+  foreignKey: 'tag_id'
+})
 
 module.exports = {
   Product,
