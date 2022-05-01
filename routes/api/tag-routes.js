73c948f490a3466a3a0bcf58add7153d5bd277c7
StevenM <stevenbmendoza@gmail.com>
2022-05-01 13:27:24 -0700

    delete tag by id

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -64,6 +64,18 @@ router.put('/:id', (req, res) => {
 
 // delete on tag by its `id` value
 router.delete('/:id', (req, res) => {
+  Tag.destroy({where: {id: req.params.id}})
+  .then(dbTagData => {
+    if (!dbTagData) {
+      res.status(404).json({ message: 'No tag found with this id' })
+      return;
+    }
+    res.json(dbTagData);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 module.exports = router;
