b67f69b2f8bf0fd014ffe7a4b2c3c61bb9138e3d
StevenM <stevenbmendoza@gmail.com>
2022-05-01 13:19:40 -0700

    post tag route

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -15,10 +15,32 @@ router.get('/', (req, res) => {
 
 // find a single tag by its `id` & associated Product data
 router.get('/:id', (req, res) => {
+  Tag.findOne({
+    where: {id: req.params.id},
+    attributes: [
+      'id',
+      'tag_name'
+    ]
+  })
+  .then(dbTagData => {
+    if (!dbTagData) {
+      res.status(404).json({ message: 'No tag with this id found'});
+      return;
+    }
+    res.json(dbTagData);
+  })
 });
 
 // create a new tag
 router.post('/', (req, res) => {
+  Tag.create({
+    tag_name: req.body.tag_name
+  })
+  .then(dbTagData => res.json(dbTagData))
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  })
 });
 
 // update a tag's name by its `id` value
