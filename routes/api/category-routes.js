7135e65da8e72a4e6e6d20537100c98492d45c40
StevenM <stevenbmendoza@gmail.com>
2022-05-01 12:06:06 -0700

    categories post route

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -40,6 +40,11 @@ router.post('/', (req, res) => {
   Category.create({
     category_name: req.body.category_name
   })
+  .then(dbCategoryData => res.json(dbCategoryData))
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 router.put('/:id', (req, res) => {
